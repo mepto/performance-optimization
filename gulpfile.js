169a88c105a8705883537149ab0e7c821cd8f8a6
Nathalie <natg.inf@gmail.com>
2016-10-20 16:14:05 +0200

    change structure

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var cleanCSS = require('gulp-clean-css')
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('views/js/*.js')
+    return gulp.src(['src/views/js/*.js','src/js/*.js'])
 //    return gulp.src('js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
@@ -28,37 +28,39 @@ gulp.task('lint', function() {
 
 // Concatenate & Minify JS
 
-gulp.task('scripts', function() {
-    return gulp.src('views/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('views/dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify().on('error', function(e) {
-        console.log(e);
-    }))
-        .pipe(gulp.dest('views/dist/js'));
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src('views/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('views/dist/css'));
-});
-
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src('css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/css'));
-});
+//gulp.task('scripts', function() {
+//    return gulp.src('views/js/*.js')
+//        .pipe(concat('all.js'))
+//        .pipe(gulp.dest('views/dist'))
+//        .pipe(rename('all.min.js'))
+//        .pipe(uglify().on('error', function(e) {
+//        console.log(e);
+//    }))
+//        .pipe(gulp.dest('views/dist/js'));
+//});
+//
+//gulp.task('minify-css', function() {
+//  return gulp.src('views/css/*.css')
+//    .pipe(cleanCSS())
+//    .pipe(concat('styles.min.css'))
+//    .pipe(gulp.dest('views/dist/css'));
+//});
+//
+//gulp.task('scripts', function() {
+//    return gulp.src('js/*.js')
+//        .pipe(concat('all.js'))
+//        .pipe(gulp.dest('dist'))
+//        .pipe(rename('all.min.js'))
+//        .pipe(uglify())
+//        .pipe(gulp.dest('dist/js'));
+//});
+//
+//gulp.task('minify-css', function() {
+//  return gulp.src('css/*.css')
+//    .pipe(cleanCSS())
+//    .pipe(concat('style.min.css'))
+//    .pipe(gulp.dest('dist/css'));
+//});
 
 //// inline css for emails
 //guld.task('inlineCss', function() {
@@ -74,7 +76,7 @@ gulp.task('minify-css', function() {
 
 //inline css, js, svg into html files
 
-gulp.src(['index.html','project-2048.html','project-mobile.html','project-webperf.html'])
+gulp.src(['src/index.html','src/project-2048.html','src/project-mobile.html','src/project-webperf.html'])
   .pipe(inline({
     //base: 'public/',
     js: uglify,
@@ -84,22 +86,24 @@ gulp.src(['index.html','project-2048.html','project-mobile.html','project-webper
   }))
   .pipe(gulp.dest('dist/'));
 
-gulp.src('views/pizza.html')
+gulp.src('src/views/pizza.html')
   .pipe(inline({
     //base: 'public/',
     js: uglify,
     css: cleanCSS,
-    disabledTypes: ['svg', 'img'], // Only inline css and js files
+   // disabledTypes: ['svg', 'img'], // Only inline css and js files
 //    ignore: ['./css/do-not-inline-me.css']
   }))
-  .pipe(gulp.dest('views/dist/'));
+  .pipe(gulp.dest('dist/views/'));
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('views/js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/views/js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/css/*.css', ['lint', 'minify-css']);
+    gulp.watch('src/views/css/*.css', ['lint', 'minify-css']);
    // gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'scripts', 'watch']); //sass inline
+gulp.task('default', ['lint', 'watch']); //sass inline 'scripts', 'minify-css',
